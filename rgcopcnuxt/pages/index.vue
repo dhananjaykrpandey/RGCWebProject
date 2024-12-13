38e83019546a697eb96c25844e79fe41747e1d61
Dhananjay Kumar Pandey <dhananjaykrpandey@gmail.com>
2024-12-13 14:25:11 +0400

    test for deploy

--- rgcopcnuxt/pages/index.vue
+++ rgcopcnuxt/pages/index.vue
@@ -1,4 +1,22 @@
 <template>
+  <div>
+    <h1>Welcome, {{ name }}!</h1>
+    <p>This is your first Nuxt 3 page.</p>
+    <button class="btn btn-info">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'User',
+    };
+  },
+};
+</script>
+
+<!-- <template>
     <div>
       <h1>Welcome, {{ name }}!</h1>
       <p>This is your first Nuxt 3 page.</p>
@@ -14,5 +32,5 @@
       };
     },
   };
-  </script>
+  </script> -->
   
--- rgcopcnuxt/plugins/bootstrap.js
+++ rgcopcnuxt/plugins/bootstrap.js
@@ -1,5 +1,26 @@
 import { defineNuxtPlugin } from "#app";
 
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (includes Popper.js)
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (includes Popper.js)
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min';
 
+// plugins/bootstrap.js
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS (including Popper.js)
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   // Make the bootstrap object globally available (if you plan to use it programmatically)
+//   nuxtApp.provide('bs', bootstrap);
+
+//   console.log('Bootstrap is loaded and available globally as $bs!');
+// });
+// plugins/bootstrap.js
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
+export default defineNuxtPlugin(async (nuxtApp) => {
+  if (process.client) {
+    const bootstrap = await import('bootstrap/dist/js/bootstrap.bundle.min'); // Dynamically import Bootstrap JS
+    nuxtApp.provide('bs', bootstrap);
+    console.log('Bootstrap JS is loaded on the client side!');
+  }
+});
